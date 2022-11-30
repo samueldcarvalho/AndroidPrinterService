80fac75db1e5c92cbef2b401283ca91d622c1058
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2022-11-30 14:45:39 -0300

    Inicialização do projeto

---
+++ android/app/src/main/kotlin/com/example/android_printer_service/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.android_printer_service
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    private String sharedText;
+    private static final String CHANNEL = "app.channel.shared.data";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+        if ("text/plain".equals(type)) {
+            handleSendText(intent); // Handle text being sent
+        }
+        }
+    }
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            if (call.method.contentEquals("getSharedText")) {
+                                result.success(sharedText);
+                                sharedText = null;
+                            }
+                        }
+                );
+    }
+
+    void handleSendText(Intent intent) {
+        sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+    }
+}
